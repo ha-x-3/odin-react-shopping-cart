67730a49922f96b15d1de135652ca0e866e13fd7
Brittney Epperson <beppers87@hotmail.com>
2024-09-12 18:44:00 -0500

    Minor styling update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
-  return (
+	return (
 		<>
-			<h1>Shopping Cart</h1>
-			<small>
-				<a
-					target='_blank'
-					href='https://icons8.com/icon/0Ri8HFYcOcBC/shopping-cart'
-				>
-					Shopping Cart
-				</a>{' '}
-				icon by{' '}
-				<a
-					target='_blank'
-					href='https://icons8.com'
-				>
-					Icons8
-				</a>
-			</small>
+			<div className='app'>
+				<h1>Shopping Cart</h1>
+				<small>
+					<a
+						target='_blank'
+						href='https://icons8.com/icon/0Ri8HFYcOcBC/shopping-cart'
+					>
+						Shopping Cart
+					</a>{' '}
+					icon by{' '}
+					<a
+						target='_blank'
+						href='https://icons8.com'
+					>
+						Icons8
+					</a>
+				</small>
+			</div>
 		</>
-  );
+	);
 }
 
-export default App
+export default App;
